65da32968556c8155e69007c11b9e78dae4e56fb
SantiagoGlez12 <54249136+SantiagoGlez12@users.noreply.github.com>
2021-10-21 19:12:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+*@author Santiago Gonzalez
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+  /**
+  *@param a Primer entero de la ecuacion
+  *@param b Segundo entero de la ecuacion
+  *@return  La suma de los enteros a más b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
